cb8d6bf37d0b49662113475778bc7072db6e3dc6
Kudryashov <hekpomamohtehok@gmail.com>
2014-03-17 14:25:10 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BarleyBreak::Application.config.secret_key_base = '466b232de9041bdf45d6acaaa1454c979dac65373ce75477c91dab26b8f994f2d43629a83c84cc63774c12d694fa7e54606ac6c26d635a1439727f68965b50e5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BarleyBreak::Application.config.session_store :cookie_store, key: '_barley-break_session'
