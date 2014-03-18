408901846a1a6f51add644611771df73e04d13a1
Kudryashov <hekpomamohtehok@gmail.com>
2014-03-18 14:45:26 +0200

    новая таблица

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,5 +1,6 @@
 class GameController < ApplicationController
   def show
-  	@array = [0..15].sort_by {rand}
+  	
+  	@array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].shuffle
   end
 end
