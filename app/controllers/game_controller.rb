99b0fb2d4e4ee8f69d0e39c4d1ae4f2a87ca3e20
Kudryashov <hekpomamohtehok@gmail.com>
2014-03-27 15:04:34 +0200

    27.03.14

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,5 @@
 class GameController < ApplicationController
   def show
-  	a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-  	c = a.shuffle
-  	@array = c
-  end
+  	@array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, nil].shuffle
+  end  
 end
